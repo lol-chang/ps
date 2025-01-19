1508cb94cd1ffd5ff9cd5572f8c65e1128418445
lol-chang <doclecsj@naver.com>
2025-01-19 20:56:56 +0900

    [book] 그래프 문제 기초 문제 풀이

--- book/graph3.py
+++ book/graph3.py
@@ -1,27 +1,26 @@
-n, m = 4,5
-res = 0
+n, m = map(int, input().split())
 
 graph = []
 for _ in range(n):
     graph.append(list(map(int, input())))
 
-
 def dfs(x,y):
-    if x < 0 or x >= n or y < 0 or y >= m:
+    if x<=-1 or x>= n or y <= -1 or y >= m:
         return False
-    
-    if graph[x][y]==0:
+
+    if graph[x][y] == 0:
         graph[x][y]=1
-        dfs(x-1,y)
-        dfs(x+1,y)
-        dfs(x,y-1)
-        dfs(x,y+1)
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
         return True
+    
     return False
-    
-    
+
+res = 0
 for i in range(n):
     for j in range(m):
         if dfs(i,j)==True:
-            res+=1
+            res += 1 
 print(res)
